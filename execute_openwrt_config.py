a69cadb29d8be8c022ebb1890a52d2b2c6eb4c9b
Agy (Agent-042) <agent-042@secureblue.local>
2026-08-01 10:15:00 -0500

    [Agy-AutoSync] Apply complete OpenWrt & host NetworkManager security blueprint

--- execute_openwrt_config.py
+++ execute_openwrt_config.py
@@ -1,85 +1,68 @@
 #!/usr/bin/env python3
 import paramiko
-import sys
-import time
 
 OPENWRT_IP = "192.168.1.1"
-PASSWORDS = ["Daddy-Cum-Zaddy!@#", "Lick-My-Ass!@#", "goodlife", "admin", "root"]
+PASSWORD = "Daddy-Cum-Zaddy!@#"
 
-def run_remote_config():
+def run_blueprint():
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    connected = False
-    active_password = None
-    
-    for pwd in PASSWORDS:
-        try:
-            print(f"[*] Attempting SSH connection to {OPENWRT_IP} with password: '{pwd}'...")
-            client.connect(OPENWRT_IP, username="root", password=pwd, timeout=4)
-            connected = True
-            active_password = pwd
-            print(f"[+] SUCCESS: Authenticated on OpenWrt with password: '{pwd}'")
-            break
-        except paramiko.AuthenticationException:
-            continue
-        except Exception as e:
-            print(f"[!] Connection attempt error: {e}")
-            continue
-            
-    if not connected:
-        print("[!] Could not authenticate on OpenWrt via SSH.")
-        return False
-        
+    print(f"[*] Connecting to OpenWrt router {OPENWRT_IP}...")
+    client.connect(OPENWRT_IP, username="root", password=PASSWORD, timeout=5)
+    print("[+] SSH Authentication Successful.")
+
     commands = """
-# 1. iPhone DHCP Static Lease
+# 1. Interface & Metric Optimization (Disable ISP DNS)
+uci set network.wan.metric='10'
+uci set network.wan.peerdns='0'
+uci set network.wwan.metric='20'
+uci set network.wwan.peerdns='0'
+uci commit network
+
+# 2. DNSMasq Hardening
+uci set dhcp.@dnsmasq[0].noresolv='0'
+uci set dhcp.@dnsmasq[0].localuse='1'
+uci set dhcp.@dnsmasq[0].strictorder='1'
+uci set dhcp.@dnsmasq[0].domainneeded='1'
+uci set dhcp.@dnsmasq[0].boguspriv='1'
+uci delete dhcp.@dnsmasq[0].server 2>/dev/null || true
+uci add_list dhcp.@dnsmasq[0].server='1.1.1.1'
+uci add_list dhcp.@dnsmasq[0].server='8.8.8.8'
+
+# 3. Static DHCP Leases (Workstation & iPhone)
 uci set dhcp.iphone=host
-uci set dhcp.iphone.name='iPhone'
+uci set dhcp.iphone.name='iphone.lan'
 uci set dhcp.iphone.mac='E6:2D:C5:95:DA:D6'
 uci set dhcp.iphone.ip='192.168.1.197'
 uci set dhcp.iphone.leasetime='infinite'
+
+uci set dhcp.workstation=host
+uci set dhcp.workstation.name='workstation.lan'
+uci set dhcp.workstation.mac='74:56:3C:3D:D7:59'
+uci set dhcp.workstation.ip='192.168.1.141'
+uci set dhcp.workstation.leasetime='infinite'
 uci commit dhcp
 
-# 2. Configure Wired WAN & Wireless Repeater Network Interfaces
-uci set network.wan=interface
-uci set network.wan.device='eth0'
-uci set network.wan.proto='dhcp'
-uci set network.wan.metric='10'
+# 4. fw4 Firewall Rules & NAT
+uci set firewall.@zone[0].input='ACCEPT'
+uci set firewall.@zone[0].forward='ACCEPT'
+uci set firewall.wan.masquerade='1'
+uci set firewall.wan.input='REJECT'
+uci set firewall.wan.forward='REJECT'
 
-uci set network.wwan=interface
-uci set network.wwan.proto='dhcp'
-uci set network.wwan.metric='20'
-uci commit network
-
-# 3. Add Repeater Client for SSID honeypot
-uci set wireless.repeater_honeypot=wifi-iface
-uci set wireless.repeater_honeypot.device='radio0'
-uci set wireless.repeater_honeypot.mode='sta'
-uci set wireless.repeater_honeypot.network='wwan'
-uci set wireless.repeater_honeypot.ssid='honeypot'
-uci set wireless.repeater_honeypot.encryption='psk2'
-uci commit wireless
-
-# 4. Restart Services
+uci commit firewall
 /etc/init.d/network restart
 /etc/init.d/dnsmasq restart
 /etc/init.d/firewall restart
 
-echo 'OPENWRT_CONFIG_APPLIED_SUCCESSFULLY'
+echo 'OPENWRT_BLUEPRINT_APPLIED_SUCCESSFULLY'
 """
-    print("[+] Executing configuration payload on OpenWrt...")
     stdin, stdout, stderr = client.exec_command(f'sh -c "{commands}"')
-    
-    out = stdout.read().decode('utf-8')
-    err = stderr.read().decode('utf-8')
-    
-    print("--- OpenWrt Output ---")
-    print(out)
-    if err:
-        print(err)
-        
+    print("--- Execution Output ---")
+    print(stdout.read().decode('utf-8'))
     client.close()
     return True
 
 if __name__ == "__main__":
-    run_remote_config()
+    run_blueprint()
